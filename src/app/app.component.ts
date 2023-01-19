c0151e1782d2ee8fd114d60536ae4dfc4bb4aad8
Augusto <augustooliveixa@gmail.com>
2023-01-19 16:30:54 -0300

    Criando functions para consumo do OpenWeatherMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,55 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WEATHERDEV';
 
+export class AppComponent implements OnInit{
 
-  nameCity = 'insira sua cidade';
+  Inputcity: any  = ''; // valor da cidade inserida
 
-  fun(): string{
-   this.nameCity = this.nameCity.toUpperCase();
-    return this.nameCity;
+  ngOnInit(): void {
 
   }
+
+  title = 'WEATHERDEV';
+
+  apikey = "7400ce0aba7ac77509dede9456051077";
+
+  jsonCity = '';
+
+  Upper(string: string){
+    let value = string.toUpperCase();
+    return value;
+  }
+
+  getWeather(){
+    var weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.Inputcity + '&appid=' + this.apikey + '&units=metric';
+    console.log(weather_url);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', weather_url, true);
+    xhr.onreadystatechange = () =>{
+      if(xhr.readyState === 4 && xhr.status === 200){
+        var weather_data = JSON.parse(xhr.responseText);
+        this.displayWeather(weather_data);
+      }
+    };
+    xhr.send();
+  }
+
+  displayWeather(weather_data: string){
+    var clima = weather_data;
+    this.jsonCity = clima;
+
+  }
+
+
+
 }
