e12b49b468bfb334d49152d2ee75765ed0547a7a
Augusto <augustooliveixa@gmail.com>
2023-01-20 18:28:15 -0300

    Criando duas functions para exibir e ocultar componente (show-hide)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,11 @@ export class AppComponent implements OnInit{
   Inputcity: any  = ''; // valor da cidade inserida
 
   ngOnInit(): void {
-    console.log(this.cidade.name);
-
   }
+
+  loading: any; // variavel de controle para exibição do 'loading'
+
+  loadingTimer: any;
 
   title = 'WEATHERDEV';
 
@@ -35,15 +37,16 @@ export class AppComponent implements OnInit{
 
   jsonCity = '';
 
+  isColorTrue: any  = true;
+
   Upper(string: string){
     let value = string.toUpperCase();
     return value;
   }
 
   getWeather(){
+    this.showLoading();
     var weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.Inputcity + '&appid=' + this.apikey + '&units=metric';
-    console.log(weather_url);
-
     var xhr = new XMLHttpRequest();
     xhr.open('GET', weather_url, true);
     xhr.onreadystatechange = () =>{
@@ -61,5 +64,23 @@ export class AppComponent implements OnInit{
     this.cidade.name = weather_data.name;
     this.cidade.pressure = weather_data.pressure;
     this.cidade.message = weather_data.message;
+
   }
+
+  // functions de loading
+  showLoading(){
+    this.loadingTimer = setTimeout(() =>{
+      this.loading = true;
+    },100);
+
+    setTimeout(() =>{
+      this.hideLoading();
+    },250)
+  }
+  hideLoading(){
+    clearTimeout(this.loadingTimer);
+    this.loading = false;
+  }
+
+
 }
